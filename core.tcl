f53e011869a8f99561759ed5d81e9b572dc7d510
omigo00 <omid.bodaghi79@gmail.com>
2020-06-17 18:56:26 +0430

    add topology

--- core.tcl
+++ core.tcl
@@ -2,7 +2,7 @@
 set ns [new Simulator]
 
 #Define different colors for data flows (for NAM)
-$ns color 1 Blue
+$ns color 1 Green
 $ns color 2 Red
 
 #Open the NAM trace file
@@ -20,35 +20,53 @@ proc finish {} {
         exit 0
 }
 
+#$defaultRNG seed 999
+
 #Create four nodes
-set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
 
-#Create links between the nodes
-$ns duplex-link $n0 $n2 2Mb 10ms DropTail
-$ns duplex-link $n1 $n2 2Mb 10ms DropTail
-$ns duplex-link $n2 $n3 1.7Mb 20ms DropTail
+set linkDelay23_ [new RandomVariable/Uniform];
+$linkDelay23_ set min_ 5 
+$linkDelay23_ set max_ 25
 
-#Set Queue Size of link (n2-n3) to 10
-$ns queue-limit $n2 $n3 10
+set linkDelay56_ [new RandomVariable/Uniform];
+$linkDelay56_ set min_ 5 
+$linkDelay56_ set max_ 25
+
+$ns duplex-link $n1 $n3 100Mb 5ms DropTail
+$ns duplex-link $n2 $n3 100Mb [expr [$linkDelay23_ value]]ms DropTail
+
+$ns duplex-link $n3 $n4 100Kb 1ms DropTail
+$ns duplex-link $n4 $n5 100Mb 5ms DropTail
+$ns duplex-link $n4 $n6 100Mb [expr [$linkDelay56_ value]]ms DropTail
+
+$ns queue-limit $n3 $n4 10
+$ns queue-limit $n4 $n5 10
+$ns queue-limit $n4 $n6 10
 
 #Give node position (for NAM)
-$ns duplex-link-op $n0 $n2 orient right-down
-$ns duplex-link-op $n1 $n2 orient right-up
-$ns duplex-link-op $n2 $n3 orient right
+$ns duplex-link-op $n1 $n3 orient left-up
+$ns duplex-link-op $n2 $n3 orient left-down
+#$ns duplex-link-op $n3 $n4 orient center
+$ns duplex-link-op $n4 $n5 orient right-up
+$ns duplex-link-op $n4 $n6 orient right-down
+#$ns duplex-link-op $n3 $n4 orient center
 
 #Monitor the queue for link (n2-n3). (for NAM)
-$ns duplex-link-op $n2 $n3 queuePos 0.5
+$ns duplex-link-op $n3 $n4 queuePos 0.5
 
 
 #Setup a TCP connection
 set tcp [new Agent/TCP]
 $tcp set class_ 2
-$ns attach-agent $n0 $tcp
+$ns attach-agent $n1 $tcp
 set sink [new Agent/TCPSink]
-$ns attach-agent $n3 $sink
+$ns attach-agent $n5 $sink
 $ns connect $tcp $sink
 $tcp set fid_ 1
 
@@ -57,39 +75,17 @@ set ftp [new Application/FTP]
 $ftp attach-agent $tcp
 $ftp set type_ FTP
 
-
-#Setup a UDP connection
-set udp [new Agent/UDP]
-$ns attach-agent $n1 $udp
-set null [new Agent/Null]
-$ns attach-agent $n3 $null
-$ns connect $udp $null
-$udp set fid_ 2
-
-#Setup a CBR over UDP connection
-set cbr [new Application/Traffic/CBR]
-$cbr attach-agent $udp
-$cbr set type_ CBR
-$cbr set packet_size_ 1000
-$cbr set rate_ 1mb
-$cbr set random_ false
-
-
 #Schedule events for the CBR and FTP agents
-$ns at 0.1 "$cbr start"
+$ns at 0.1 "$ftp start"
 $ns at 1.0 "$ftp start"
 $ns at 4.0 "$ftp stop"
-$ns at 4.5 "$cbr stop"
+$ns at 4.5 "$ftp stop"
 
 #Detach tcp and sink agents (not really necessary)
-$ns at 4.5 "$ns detach-agent $n0 $tcp ; $ns detach-agent $n3 $sink"
+$ns at 4.5 "$ns detach-agent $n1 $tcp ; $ns detach-agent $n5 $sink"
 
 #Call the finish procedure after 5 seconds of simulation time
 $ns at 5.0 "finish"
-
-#Print CBR packet size and interval
-puts "CBR packet size = [$cbr set packet_size_]"
-puts "CBR interval = [$cbr set interval_]"
 
 #Run the simulation
 $ns run
